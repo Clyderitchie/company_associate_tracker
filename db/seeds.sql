d73c121884f8cc506a1768eb66fc49db2f161b7d
francis <clyderitchie@yahoo.com>
2023-11-08 19:33:56 -0800

    Added my tables in the schema file as well as added some data to the seeds file to populate information into the tables

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (department_name) 
+VALUES 
+('Engineering'), 
+('Finance'), 
+('Legal'), 
+('Sales');
+
+INSERT INTO role (title, salary)
+VALUES 
+('Sales Lead', '100000'),
+('Salesperson', '80000'),
+('Lead Engineer', '150000'),
+('Software Engineer', '120000'),
+('Account Manager', '160000'),
+('Accountant', '125000'),
+('Legal Team Lead', '25000'),
+('Lawyer', '190000');
+
+INSERT INTO employee (first_name, last_name)
+VALUES 
+('Clyde','Ritchie'),
+('Val','Fry'),
+('Apollo','Smith'),
+('Dylan','Miller'),
+('Ed','Reed'),
+('Conny','Francis'),
+('Andrew','Hicks'),
+('Robert','David');
